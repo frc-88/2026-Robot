7a18e945ac17e133964c599081bd185116bf8a93
Recreated3684 <DStewart3684@gmail.com>
2026-02-01 16:40:24 -0500

    Add telemetry to Intake subsystem for supply current, motor voltage, and power consumption

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,20 +1,25 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.VelocityDutyCycle;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.util.preferenceconstants.DoublePreferenceConstant;
+import org.littletonrobotics.junction.Logger;
 
 public class Intake extends SubsystemBase {
 
   private final TalonFX intake = new TalonFX(Constants.INTAKE_MAIN, CANBus.roboRIO());
+  private final var supplyCurrent = intake.getSupplyCurrent();
+  private final var motorVoltage = intake.getMotorVoltage();
   private VelocityDutyCycle request = new VelocityDutyCycle(0.0);
   private DutyCycleOut requestcycle = new DutyCycleOut(0.0);
 
@@ -22,6 +27,22 @@ public class Intake extends SubsystemBase {
 
   public Intake() {
     configureTalons();
+  }
+
+  @Override
+  public void periodic() {
+    BaseStatusSignal.refreshAll(supplyCurrent, motorVoltage);
+    double supplyCurrentAmps = supplyCurrent.getValueAsDouble();
+    double voltage = motorVoltage.getValueAsDouble();
+    double powerWatts = voltage * supplyCurrentAmps;
+
+    Logger.recordOutput("Intake/SupplyCurrentAmps", supplyCurrentAmps);
+    Logger.recordOutput("Intake/PowerWatts", powerWatts);
+    Logger.recordOutput("Intake/MotorVoltage", voltage);
+
+    SmartDashboard.putNumber("Intake/SupplyCurrentAmps", supplyCurrentAmps);
+    SmartDashboard.putNumber("Intake/PowerWatts", powerWatts);
+    SmartDashboard.putNumber("Intake/MotorVoltage", voltage);
   }
 
   private void configureTalons() {
