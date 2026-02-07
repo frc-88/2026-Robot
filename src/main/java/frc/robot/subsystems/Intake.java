32e455b5c210186e96f1cee44ac416accd0c54ff
TJ Squared <navtges@gmail.com>
2026-02-07 14:15:46 -0500

    pid for intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,49 +2,62 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.DutyCycleOut;
-import com.ctre.phoenix6.controls.VelocityDutyCycle;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.util.preferenceconstants.DoublePreferenceConstant;
+import frc.robot.util.preferenceconstants.MotionMagicPIDPreferenceConstants;
+import java.util.function.DoubleSupplier;
 
 public class Intake extends SubsystemBase {
+  private final TalonFX intakeRoller =
+      new TalonFX(Constants.INTAKE_ROLLER, new CANBus("Drivetrain"));
+  // private final TalonFX intakePivot = new TalonFX(Constants.INTAKE_PIVOT, CANBus.roboRIO());
 
-  private final TalonFX intake = new TalonFX(Constants.INTAKE_MAIN, CANBus.roboRIO());
-  private VelocityDutyCycle request = new VelocityDutyCycle(0.0);
-  private DutyCycleOut requestcycle = new DutyCycleOut(0.0);
+  private VelocityVoltage request = new VelocityVoltage(0.0);
+  private MotionMagicVoltage pivotRequest = new MotionMagicVoltage(0.0);
 
-  private DoublePreferenceConstant speed = new DoublePreferenceConstant("Intake/Speed", 0.8);
+  private DoublePreferenceConstant intakeSpeed = new DoublePreferenceConstant("Intake/Speed", 0.8);
+
+  private MotionMagicPIDPreferenceConstants intakeConfigConstants =
+      new MotionMagicPIDPreferenceConstants("IntakeMotors");
 
   public Intake() {
     configureTalons();
   }
 
-  private void configureTalons() {
-    TalonFXConfiguration config = new TalonFXConfiguration();
-    // config.Slot0.kP =
-    // config.Slot0.kI =
-    // config.Slot0.kD =
-    // config. =
-    config.MotorOutput.Inverted =
-        InvertedValue.CounterClockwise_Positive; // this might not work yet
-    intake.getConfigurator().apply(config);
+  public void periodic() {
+    SmartDashboard.putNumber(
+        "Intake/RollerVelocity", intakeRoller.getVelocity().getValueAsDouble());
   }
 
-  private void setSpeed(double speed) {
-    intake.setControl(requestcycle.withOutput(speed));
+  private void configureTalons() {
+    TalonFXConfiguration intakeConfig = new TalonFXConfiguration();
+    intakeConfig.Slot0.kP = intakeConfigConstants.getKP().getValue();
+    intakeConfig.Slot0.kI = intakeConfigConstants.getKI().getValue();
+    intakeConfig.Slot0.kD = intakeConfigConstants.getKD().getValue();
+    intakeConfig.Slot0.kV = intakeConfigConstants.getKV().getValue();
+    intakeConfig.Slot0.kS = intakeConfigConstants.getKS().getValue();
+    intakeConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+    intakeRoller.getConfigurator().apply(intakeConfig);
+  }
+
+  private void setSpeed(DoubleSupplier speed) {
+    intakeRoller.setControl(request.withVelocity(speed.getAsDouble()));
   }
 
   private void stopMotors() {
-    intake.stopMotor();
+    intakeRoller.stopMotor();
   }
 
-  public Command runIndexer() {
-    return new RunCommand(() -> setSpeed(speed.getValue()), this);
+  public Command runIntake() {
+    return new RunCommand(() -> setSpeed(() -> intakeSpeed.getValue()), this);
   }
 
   public Command stopIntake() {
