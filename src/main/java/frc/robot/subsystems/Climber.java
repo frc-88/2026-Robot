159d30bbf2088c534fddd78f6540b1207cd91b48
Nick Avtges <navtges@gmail.com>
2026-02-07 15:49:02 -0500

    climber sysid routines

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,18 +1,25 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Volts;
+
 import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.configs.CANrangeConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.CANrange;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.math.filter.LinearFilter;
+import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.Constants;
 import frc.robot.util.preferenceconstants.MotionMagicPIDPreferenceConstants;
 
@@ -29,6 +36,28 @@ public class Climber extends SubsystemBase {
 
   private final MotionMagicVoltage liftMotionMagic = new MotionMagicVoltage(0);
   private final MotionMagicVoltage pivotMotionMagic = new MotionMagicVoltage(0);
+  private final VoltageOut liftVoltage = new VoltageOut(0.0);
+  private final VoltageOut pivotVoltage = new VoltageOut(0.0);
+
+  private final SysIdRoutine liftIdRoutine =
+      new SysIdRoutine(
+          new SysIdRoutine.Config(
+              null, // Use default ramp rate (1 V/s)
+              Volts.of(4), // Reduce dynamic step voltage to 4 to prevent brownout
+              null, // Use default timeout (10 s)
+              // Log state with Phoenix SignalLogger class
+              (state) -> SignalLogger.writeString("state", state.toString())),
+          new SysIdRoutine.Mechanism(this::setLiftVoltage, null, this));
+
+  private final SysIdRoutine pivotIdRoutine =
+      new SysIdRoutine(
+          new SysIdRoutine.Config(
+              null, // Use default ramp rate (1 V/s)
+              Volts.of(4), // Reduce dynamic step voltage to 4 to prevent brownout
+              null, // Use default timeout (10 s)
+              // Log state with Phoenix SignalLogger class
+              (state) -> SignalLogger.writeString("state", state.toString())),
+          new SysIdRoutine.Mechanism(this::setPivotVoltage, null, this));
 
   private final LinearFilter f = LinearFilter.singlePoleIIR(0.1, 0.02);
   private final Debouncer debouncer = new Debouncer(0.15);
@@ -37,6 +66,7 @@ public class Climber extends SubsystemBase {
   public Climber() {
     configureCANrange();
     configureMotors();
+    configureSmartDashboardButtons();
   }
 
   private void configureCANrange() {
@@ -73,6 +103,23 @@ public class Climber extends SubsystemBase {
     pivot.getConfigurator().apply(liftConfig);
   }
 
+  private void configureSmartDashboardButtons() {
+    SmartDashboard.putData(
+        "Climber/Lift/SysId/Quasistatic Forward", liftSysIdQuasistatic(Direction.kForward));
+    SmartDashboard.putData(
+        "Climber/Lift/SysId/Quasistatic Reverse", liftSysIdQuasistatic(Direction.kReverse));
+    SmartDashboard.putData(
+        "Climber/Pivot/SysId/Quasistatic Forward", pivotSysIdQuasistatic(Direction.kForward));
+    SmartDashboard.putData(
+        "Climber/Pivot/SysId/Quasistatic Reverse", pivotSysIdQuasistatic(Direction.kReverse));
+    SmartDashboard.putData(
+        "Climber/Lift/SysId/Dynamic Forward", liftSysIdDynamic(Direction.kForward));
+    SmartDashboard.putData(
+        "Climber/Lift/SysId/Dynamic Reverse", liftSysIdDynamic(Direction.kReverse));
+    SmartDashboard.putData("Shooter/SysId/Dynamic Forward", pivotSysIdDynamic(Direction.kForward));
+    SmartDashboard.putData("Shooter/SysId/Dynamic Reverse", pivotSysIdDynamic(Direction.kReverse));
+  }
+
   public boolean isDetected() {
     double distance = f.calculate(canRange.getDistance().getValueAsDouble());
     if (debouncer.calculate(canRange.getSignalStrength().getValueAsDouble() > 50000) == false) {
@@ -103,8 +150,17 @@ public class Climber extends SubsystemBase {
     pivot.setControl(pivotMotionMagic.withPosition(position));
   }
 
+  private void setLiftVoltage(Voltage volts) {
+    lift.setControl(liftVoltage.withOutput(volts));
+  }
+
+  private void setPivotVoltage(Voltage volts) {
+    lift.setControl(pivotVoltage.withOutput(volts));
+  }
+
   public void periodic() {
-    SmartDashboard.putNumber("Climber/CANrange/Distance", f.calculate(canRange.getDistance().getValueAsDouble()));
+    SmartDashboard.putNumber(
+        "Climber/CANrange/Distance", f.calculate(canRange.getDistance().getValueAsDouble()));
     SmartDashboard.putBoolean("Climber/CANrange/Detected", isDetected());
     SmartDashboard.putNumber("Climber/Lift/Position", lift.getPosition().getValueAsDouble());
     SmartDashboard.putNumber("Climber/Lift/Velocity", lift.getVelocity().getValueAsDouble());
@@ -122,5 +178,21 @@ public class Climber extends SubsystemBase {
 
   public Command pivotGoto(double position) {
     return new RunCommand(() -> pivotGotoPosition(position), this);
+  }
+
+  public Command liftSysIdQuasistatic(SysIdRoutine.Direction direction) {
+    return liftIdRoutine.quasistatic(direction);
+  }
+
+  public Command pivotSysIdQuasistatic(SysIdRoutine.Direction direction) {
+    return pivotIdRoutine.quasistatic(direction);
+  }
+
+  public Command liftSysIdDynamic(SysIdRoutine.Direction direction) {
+    return liftIdRoutine.dynamic(direction);
+  }
+
+  public Command pivotSysIdDynamic(SysIdRoutine.Direction direction) {
+    return pivotIdRoutine.dynamic(direction);
   }
 }
