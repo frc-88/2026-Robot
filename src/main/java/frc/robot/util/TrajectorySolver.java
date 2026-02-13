15e4d4883e3a8b09a82d871a1a4fd192bf00a9ab
AymenAbbassi <aymenna2026@bridge-rayn.org>
2026-02-13 15:56:48 -0500

    avoid java integer math

--- src/main/java/frc/robot/util/TrajectorySolver.java
+++ src/main/java/frc/robot/util/TrajectorySolver.java
@@ -4,19 +4,19 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 
 public class TrajectorySolver {
-  public Translation2d robotToTurret = new Translation2d(0.3, -3 / 4 * Math.PI); // m
-  public Translation2d goalPosition = new Translation2d(2, 2);
-  public static Rotation2d quarterRotation = Rotation2d.fromRadians(Math.PI / 2);
+  public Translation2d robotToTurret = new Translation2d(0.3, -3.0 / 4.0 * Math.PI); // m
+  public Translation2d goalPosition = new Translation2d(2.0, 2.0);
+  public static Rotation2d quarterRotation = Rotation2d.fromRadians(Math.PI / 2.0);
 
   // TO GO IN CONSTANTS ^^
 
-  public Translation2d robotPosition = new Translation2d(1, 1); // m
-  public Rotation2d robotYaw = new Rotation2d(Math.PI * (1 / 3)); // rad
+  public Translation2d robotPosition = new Translation2d(1.0, 1.0); // m
+  public Rotation2d robotYaw = new Rotation2d(Math.PI * (1.0 / 3.0)); // rad
   public Translation2d robotVelocity = new Translation2d(); // m/s
   public double robotRotationalVelocity = 0.1; // rad/s
 
   private boolean hasPreviousTimeOfFlightGuess = false;
-  private double timeOfFlight = 0; // seconds
+  private double timeOfFlight = 0.0; // seconds
   private Translation2d turretToProjectedTarget; // m; distanceToTarget
   private double turretToProjectedTargetDistance;
   private int numberOfIterations = 5;
@@ -25,7 +25,7 @@ public class TrajectorySolver {
   public double shootSpeed;
 
   public void periodic() {
-    if (robotVelocity.getNorm() > (1 / 25)) {
+    if (robotVelocity.getNorm() > (1.0 / 25.0)) {
       newton();
     } else {
       hasPreviousTimeOfFlightGuess = false;
@@ -55,7 +55,7 @@ public class TrajectorySolver {
       timeOfFlight =
           timeOfFlight
               - (timeOfFlight - lookupTime(turretToProjectedTargetDistance))
-                  / (1
+                  / (1.0
                       - (lookupTimePrime(turretToProjectedTargetDistance))
                           * (turretToProjectedTarget.dot(
                                   turretRelativeVelocityToTarget.unaryMinus())
@@ -73,14 +73,14 @@ public class TrajectorySolver {
   }
 
   public double lookupTimePrime(double distance) {
-    return 1 / (2 * Math.sqrt(distance)); // temp
+    return 1.0 / (2.0 * Math.sqrt(distance)); // temp
   }
 
   public double lookupAngle(double distance) {
-    return distance * 25; // very temp
+    return distance * 25.0; // very temp
   }
 
   public double lookupSpeed(double distance) {
-    return distance * 30; // temp
+    return distance * 30.0; // temp
   }
 }
