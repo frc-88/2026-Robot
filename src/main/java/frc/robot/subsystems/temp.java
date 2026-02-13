152db4a6c23e71ce4ac6c2530a6fcc21a8f9ce3d
AymenAbbassi <aymenna2026@bridge-rayn.org>
2026-02-12 21:22:27 -0500

    more TOF

--- src/main/java/frc/robot/subsystems/temp.java
+++ src/main/java/frc/robot/subsystems/temp.java
@@ -14,16 +14,22 @@ public class temp {
     public Translation2d robotVelocity = new Translation2d(); //m/s
     public double robotRotationalVelocity = 0.1; //rad/s
     
-    public double timeOfFlight; //seconds
+    public double timeOfFlight = 0; //seconds
     public Translation2d turretToTarget; //m; distanceToTarget
     public int numberOfIterations = 5;
 
 
     public void newton() {
+        Translation2d a = goalPosition.minus(robotPosition).minus(robotToTurret.rotateBy(robotYaw)); //position component
+        //Translation2d b = robotVelocity.plus(robotToTurret.rotateBy((Math.PI / 2) + robotYaw).times(robotRotationalVelocity));
+
+        if (timeOfFlight==0) {
+            timeOfFlight = lookup(a.getNorm());
+        }
         int i;
         for (i=0; i<numberOfIterations; i++) {
-            Translation2d a = goalPosition.minus(robotPosition).minus(robotToTurret.rotateBy(robotYaw)); //position component
-            //Translation2d b = robotVelocity.plus();
+            //Translation2d projectedDistance = a.plus(b.times(timeOfFlight))
+            //timeOfFlight = timeOfFlight - (timeOfFlight - lookup(projectedDistance.getNorm()))/(1 - (lookupPrime(projectedDistance.getNorm()))*)
             }
 
     public double lookup(double distance) {
