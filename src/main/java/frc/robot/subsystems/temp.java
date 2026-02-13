e9c38cc942d87a76d9a087282b0081bc38d7ab63
AymenAbbassi <aymenna2026@bridge-rayn.org>
2026-02-13 14:26:21 -0500

    TOF Boom Done

--- src/main/java/frc/robot/subsystems/temp.java
+++ src/main/java/frc/robot/subsystems/temp.java
@@ -6,7 +6,8 @@ import edu.wpi.first.math.geometry.Translation2d;
 public class temp {
     public Translation2d robotToTurret = new Translation2d(0.3, -3/4 * Math.PI); //m
     public Translation2d goalPosition = new Translation2d(2, 2);
-    
+    public static Rotation2d quarterRotation = Rotation2d.fromRadians(Math.PI/2);
+
     //TO GO IN CONSTANTS ^^
 
     public Translation2d robotPosition = new Translation2d(1,1); //m
@@ -18,19 +19,32 @@ public class temp {
     public Translation2d turretToTarget; //m; distanceToTarget
     public int numberOfIterations = 5;
 
+    public void periodic() {
+        if (robotVelocity.getNorm() > (1/25)) {
+            newton();
+        }
+        else {
+            timeOfFlight = 0;
+        }
+    }
 
     public void newton() {
-        Translation2d a = goalPosition.minus(robotPosition).minus(robotToTurret.rotateBy(robotYaw)); //position component
-        //Translation2d b = robotVelocity.plus(robotToTurret.rotateBy((Math.PI / 2) + robotYaw).times(robotRotationalVelocity));
-
+        Translation2d turretDistanceToTarget = goalPosition
+            .minus(robotPosition).minus(robotToTurret.rotateBy(robotYaw));
+        Translation2d turretRelativeVelocityToTarget = robotVelocity
+            .plus(robotToTurret.rotateBy(robotYaw.plus(quarterRotation)).times(robotRotationalVelocity));
         if (timeOfFlight==0) {
-            timeOfFlight = lookup(a.getNorm());
+            timeOfFlight = lookup(turretDistanceToTarget.getNorm());
         }
         int i;
         for (i=0; i<numberOfIterations; i++) {
-            //Translation2d projectedDistance = a.plus(b.times(timeOfFlight))
-            //timeOfFlight = timeOfFlight - (timeOfFlight - lookup(projectedDistance.getNorm()))/(1 - (lookupPrime(projectedDistance.getNorm()))*)
+            Translation2d projectedDistance = turretDistanceToTarget.plus(turretRelativeVelocityToTarget.times(timeOfFlight));
+            timeOfFlight = timeOfFlight -
+            (timeOfFlight - lookup(projectedDistance.getNorm()))
+            /(1 - (lookupPrime(projectedDistance.getNorm()))*
+            (projectedDistance.dot(turretRelativeVelocityToTarget.unaryMinus())/projectedDistance.getNorm()));
             }
+    }
 
     public double lookup(double distance) {
         return Math.sqrt(distance);
