d1283eb9d82e4ee1e59cf0f1391794658bb51387
Nick Avtges <navtges@gmail.com>
2026-03-06 15:21:41 -0500

    clean the hot tub

--- src/main/java/frc/robot/subsystems/HotTub.java
+++ src/main/java/frc/robot/subsystems/HotTub.java
@@ -5,28 +5,34 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.VelocityDutyCycle;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.util.Util;
 import frc.robot.util.preferenceconstants.DoublePreferenceConstant;
 import frc.robot.util.preferenceconstants.MotionMagicPIDPreferenceConstants;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
+import org.littletonrobotics.junction.AutoLogOutput;
 
 public class HotTub extends SubsystemBase {
-  private final TalonFX spinner = new TalonFX(Constants.SPINNER_MAIN, CANBus.roboRIO());
+  // motors & devices
+  private final TalonFX m_spinner = new TalonFX(Constants.SPINNER_MAIN, CANBus.roboRIO());
 
-  private final VelocityDutyCycle request = new VelocityDutyCycle(0.0);
+  // output requests
+  private final VelocityDutyCycle m_request = new VelocityDutyCycle(0.0);
 
-  private final DoublePreferenceConstant spinnerSpeed =
+  // preferences
+  private final DoublePreferenceConstant p_spinnerSpeed =
       new DoublePreferenceConstant("Spinner/SpinnerSpeed", 0.0);
-  private final MotionMagicPIDPreferenceConstants spinnerConfigConstants =
+  private final MotionMagicPIDPreferenceConstants p_spinnerConfigConstants =
       new MotionMagicPIDPreferenceConstants("Spinner/SpinnerMotors");
 
-  private BooleanSupplier m_turretOnTarget;
+  private final BooleanSupplier m_turretOnTarget;
 
   public HotTub(BooleanSupplier turretOnTarget) {
     m_turretOnTarget = turretOnTarget;
@@ -38,35 +44,45 @@ public class HotTub extends SubsystemBase {
 
   private void configureTalons() {
     TalonFXConfiguration spinnerConfig = new TalonFXConfiguration();
-    spinnerConfig.Slot0.kP = spinnerConfigConstants.getKP().getValue();
-    spinnerConfig.Slot0.kI = spinnerConfigConstants.getKI().getValue();
-    spinnerConfig.Slot0.kD = spinnerConfigConstants.getKD().getValue();
-    spinnerConfig.Slot0.kV = spinnerConfigConstants.getKV().getValue();
-    spinnerConfig.Slot0.kS = spinnerConfigConstants.getKS().getValue();
+    spinnerConfig.Slot0.kP = p_spinnerConfigConstants.getKP().getValue();
+    spinnerConfig.Slot0.kI = p_spinnerConfigConstants.getKI().getValue();
+    spinnerConfig.Slot0.kD = p_spinnerConfigConstants.getKD().getValue();
+    spinnerConfig.Slot0.kV = p_spinnerConfigConstants.getKV().getValue();
+    spinnerConfig.Slot0.kS = p_spinnerConfigConstants.getKS().getValue();
     spinnerConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-    spinner.getConfigurator().apply(spinnerConfig);
+    m_spinner.getConfigurator().apply(spinnerConfig);
+  }
+
+  @AutoLogOutput
+  private Voltage getVoltage() {
+    return m_spinner.getMotorVoltage().getValue();
+  }
+
+  @AutoLogOutput
+  private Current getCurrent() {
+    return m_spinner.getTorqueCurrent().getValue();
+  }
+
+  @AutoLogOutput
+  private AngularVelocity getVelocity() {
+    return m_spinner.getVelocity().getValue();
   }
 
   private void setSpinnerSpeed(DoubleSupplier speed) {
-    spinner.setControl(request.withVelocity(speed.getAsDouble()));
+    m_spinner.setControl(m_request.withVelocity(speed.getAsDouble()));
   }
 
   private void stopSpinnerMotors() {
-    spinner.stopMotor();
+    m_spinner.stopMotor();
   }
 
-  public void periodic() {
-    if (Util.logif()) {
-      SmartDashboard.putNumber("Spinner/SpinnerVelocity", spinner.getVelocity().getValueAsDouble());
-      SmartDashboard.putNumber(
-          "Spinner/SpinnerCurrent", spinner.getTorqueCurrent().getValueAsDouble());
-    }
-  }
+  public void periodic() {}
 
   public Command runSpinner() {
     return new RunCommand(
         () ->
-            setSpinnerSpeed(() -> m_turretOnTarget.getAsBoolean() ? spinnerSpeed.getValue() : 0.0),
+            setSpinnerSpeed(
+                () -> m_turretOnTarget.getAsBoolean() ? p_spinnerSpeed.getValue() : 0.0),
         this);
   }
 
