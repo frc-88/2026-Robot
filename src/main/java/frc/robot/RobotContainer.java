c349e030a818cec21dde016d3d8c36d9844d9316
Matthew V <35playfun@gmail.com>
2026-03-05 19:26:56 -0500

    Improve new corrected auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -140,14 +140,9 @@ public class RobotContainer {
     hood = new Hood(trajectorySolver::getAngle);
     shooter = new Shooter(trajectorySolver::getShootSpeed);
 
-    
     // Intake:
     NamedCommands.registerCommand("Deploy Intake", intake.deployIntake());
-    NamedCommands.registerCommand(
-        "Retract Intake", intake.retractIntake());
-
-    NamedCommands.registerCommand("Start Intake", new WaitCommand(0.5));
-    NamedCommands.registerCommand("Stop Intake", new WaitCommand(0.5));
+    NamedCommands.registerCommand("Retract Intake", intake.retractIntake());
 
     // Shooter:
     NamedCommands.registerCommand("Start Shooter", shoot());
@@ -161,7 +156,6 @@ public class RobotContainer {
     // Other:
     NamedCommands.registerCommand("Calibrate Hood", hood.calibrate());
     NamedCommands.registerCommand("Reset Batman", resetBatman());
-
 
     autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
 
