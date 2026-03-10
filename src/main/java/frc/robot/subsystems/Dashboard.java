51b5b68085bc68b17c6a80f58929d7148c08e1d3
Matthew <35playfun@gmail.com>
2026-03-09 20:33:43 -0400

    Move smart dashboard calls to end of periodic

--- src/main/java/frc/robot/subsystems/Dashboard.java
+++ src/main/java/frc/robot/subsystems/Dashboard.java
@@ -21,114 +21,138 @@ public class Dashboard extends SubsystemBase {
     double gameTime = DriverStation.getMatchTime();
 
     double gameTimeInt = Math.floor(gameTime); // Round the seconds down
-    String gameTimeString = String.valueOf(Math.floor(gameTimeInt / 60)) + insertZero(String.valueOf(gameTimeInt % 60));
+    String gameTimeString =
+        String.valueOf(Math.floor(gameTimeInt / 60)) + insertZero(String.valueOf(gameTimeInt % 60));
 
-    SmartDashboard.putString("Match Time", gameTimeString);
+    SmartDashboard.putString("Driver Dashboard/Match Time", gameTimeString);
 
 
-    if(gameData.length() > 0)
-    {
+    boolean isHubActive = true;
+    String currentMatchPeriod = "unknown";
+    double periodTimeRemaining = 0;
+
+
+    if (gameData.length() > 0) {
       if (gameTime > 130) { // ---------- TRANSITION SHIFT ----------
-        SmartDashboard.putBoolean("Hub Active", true);
-        SmartDashboard.putString("Current Period", "TRANSITION SHIFT");
-        SmartDashboard.putNumber("Period Time", gameTimeInt-130);
+        isHubActive = true;
+        currentMatchPeriod = "TRANSITION SHIFT";
+        periodTimeRemaining = gameTimeInt - 130;
 
       } else if (gameTime > 105) { // ---------- SHIFT 1 ----------
-        SmartDashboard.putString("Current Period", "SHIFT 1");
-        SmartDashboard.putNumber("Period Time", gameTimeInt-105);
-        switch (gameData.charAt(0))
-        {
-          case 'B' : //Blue case code (blue won autonomous, so red goes first)
-            if (weAreRed) { SmartDashboard.putBoolean("Hub Active", true);
-            } else { SmartDashboard.putBoolean("Hub Active", false); }
+        currentMatchPeriod = "SHIFT 1";
+        periodTimeRemaining = gameTimeInt - 105;
+        switch (gameData.charAt(0)) {
+          case 'B': // Blue case code (blue won autonomous, so red goes first)
+            if (weAreRed) {
+              isHubActive = true;
+            } else {
+              isHubActive = false;
+            }
             break;
-          case 'R' : //Red case code (red won autonomous, so blue goes first)
-          if (weAreRed) { SmartDashboard.putBoolean("Hub Active", false);
-            } else { SmartDashboard.putBoolean("Hub Active", true); }
+          case 'R': // Red case code (red won autonomous, so blue goes first)
+            if (weAreRed) {
+              isHubActive = false;
+            } else {
+              isHubActive = true;
+            }
             break;
-          default : //This is corrupt data
-            SmartDashboard.putBoolean("Hub Active", true);
-            break;
-        }
-      }
-
-      else if (gameTime > 80) { // ---------- SHIFT 2 ----------
-        SmartDashboard.putString("Match Period", "SHIFT 2");
-        SmartDashboard.putNumber("Period Time", gameTimeInt-80);
-        switch (gameData.charAt(0))
-        {
-          case 'B' : //Blue case code (blue won autonomous, so red goes first)
-            if (weAreRed) { SmartDashboard.putBoolean("Hub Active", false);
-            } else { SmartDashboard.putBoolean("Hub Active", true); }
-            break;
-          case 'R' : //Red case code (red won autonomous, so blue goes first)
-          if (weAreRed) { SmartDashboard.putBoolean("Hub Active", true);
-            } else { SmartDashboard.putBoolean("Hub Active", false); }
-            break;
-          default : //This is corrupt data
-            SmartDashboard.putBoolean("Hub Active", true);
+          default: // This is corrupt data
+            isHubActive = true;
             break;
         }
-      }
-
-      else if (gameTime > 55) { // ---------- SHIFT 3 ----------
-        SmartDashboard.putString("Current Period", "SHIFT 3");
-        SmartDashboard.putNumber("Period Time", gameTimeInt-5);
-        switch (gameData.charAt(0))
-        {
-          case 'B' : //Blue case code (blue won autonomous, so red goes first)
-            if (weAreRed) { SmartDashboard.putBoolean("Hub Active", true);
-            } else { SmartDashboard.putBoolean("Hub Active", false); }
+      } else if (gameTime > 80) { // ---------- SHIFT 2 ----------
+        currentMatchPeriod = "SHIFT 2";
+        periodTimeRemaining = gameTimeInt - 80;
+        switch (gameData.charAt(0)) {
+          case 'B': // Blue case code (blue won autonomous, so red goes first)
+            if (weAreRed) {
+              isHubActive = false;
+            } else {
+              isHubActive = true;
+            }
             break;
-          case 'R' : //Red case code (red won autonomous, so blue goes first)
-          if (weAreRed) { SmartDashboard.putBoolean("Hub Active", false);
-            } else { SmartDashboard.putBoolean("Hub Active", true); }
+          case 'R': // Red case code (red won autonomous, so blue goes first)
+            if (weAreRed) {
+              isHubActive = true;
+            } else {
+              isHubActive = false;
+            }
             break;
-          default : //This is corrupt data
-            SmartDashboard.putBoolean("Hub Active", true);
-            break;
-        }
-      }
-
-      else if (gameTime > 30) { // ---------- SHIFT 4 ----------
-        SmartDashboard.putString("Match Period", "SHIFT 4");
-        SmartDashboard.putNumber("Period Time", gameTimeInt-30);
-        switch (gameData.charAt(0))
-        {
-          case 'B' : //Blue case code (blue won autonomous, so red goes first)
-            if (weAreRed) { SmartDashboard.putBoolean("Hub Active", false);
-            } else { SmartDashboard.putBoolean("Hub Active", true); }
-            break;
-          case 'R' : //Red case code (red won autonomous, so blue goes first)
-          if (weAreRed) { SmartDashboard.putBoolean("Hub Active", true);
-            } else { SmartDashboard.putBoolean("Hub Active", false); }
-            break;
-          default : //This is corrupt data
-            SmartDashboard.putBoolean("Hub Active", true);
+          default: // This is corrupt data
+            isHubActive = true;
             break;
         }
+      } else if (gameTime > 55) { // ---------- SHIFT 3 ----------
+        currentMatchPeriod = "SHIFT 3";
+        periodTimeRemaining = gameTimeInt - 5;
+        switch (gameData.charAt(0)) {
+          case 'B': // Blue case code (blue won autonomous, so red goes first)
+            if (weAreRed) {
+              isHubActive = true;
+            } else {
+              isHubActive = false;
+            }
+            break;
+          case 'R': // Red case code (red won autonomous, so blue goes first)
+            if (weAreRed) {
+              isHubActive = false;
+            } else {
+              isHubActive = true;
+            }
+            break;
+          default: // This is corrupt data
+            isHubActive = true;
+            break;
+        }
+      } else if (gameTime > 30) { // ---------- SHIFT 4 ----------
+        currentMatchPeriod = "SHIFT 4";
+        periodTimeRemaining = gameTimeInt - 30;
+        switch (gameData.charAt(0)) {
+          case 'B': // Blue case code (blue won autonomous, so red goes first)
+            if (weAreRed) {
+              isHubActive = false;
+            } else {
+              isHubActive = true;
+            }
+            break;
+          case 'R': // Red case code (red won autonomous, so blue goes first)
+            if (weAreRed) {
+              isHubActive = true;
+            } else {
+              isHubActive = false;
+            }
+            break;
+          default: // This is corrupt data
+            isHubActive = true;
+            break;
+        }
+      } else { // ---------- END GAME ----------
+        currentMatchPeriod = "END GAME";
+        periodTimeRemaining = gameTimeInt;
+        isHubActive = true;
       }
 
-      else { // ---------- END GAME ----------
-        SmartDashboard.putString("Match Period", "END GAME");
-        SmartDashboard.putNumber("Period Time", gameTimeInt);
-        SmartDashboard.putBoolean("Hub Active", true);
-      }
-
-
-    } else { //Code for no data received yet
-      SmartDashboard.putBoolean("Hub Active", true);
+    } else { // Code for no data received yet
+      isHubActive = true;
       if (gameTime < 20) { // In autonomous
-        SmartDashboard.putString("Current Period", "AUTONOMOUS");
-        SmartDashboard.putNumber("Period Time", gameTimeInt);
+        currentMatchPeriod = "AUTONOMOUS";
+        periodTimeRemaining = gameTimeInt;
       } else { // Probably in transition shift
-        SmartDashboard.putString("Current Period", "TRANSITION SHIFT");
-        SmartDashboard.putNumber("Period Time", gameTimeInt-130);
+        currentMatchPeriod = "TRANSITION SHIFT";
+        periodTimeRemaining = gameTimeInt - 130;
       }
     }
+
+    SmartDashboard.putBoolean("Driver Dashboard/Hub Active", isHubActive);
+    SmartDashboard.putString("Driver Dashboard/Current Match Period", currentMatchPeriod);
+    SmartDashboard.putNumber("Driver Dashboard/Period Time", periodTimeRemaining);
+
   }
 
-  private String insertZero(String startNumber) { // If a number (in string format) is single-digit, insert a zero to make it two digits.
+  private String insertZero(
+      String
+          startNumber) { // If a number (in string format) is single-digit, insert a zero to make it
+    // two digits.
     if (startNumber.length() < 2) {
       return "0" + startNumber;
     }
