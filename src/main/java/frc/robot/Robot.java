9745f61f6cce8d4c778bada1b2245a77222fec90
TJ Squared <navtges@gmail.com>
2026-04-10 18:20:58 -0400

    working now

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -99,7 +99,7 @@ public class Robot extends LoggedRobot {
   /** This function is called periodically when disabled. */
   @Override
   public void disabledPeriodic() {
-    // m_robotContainer.disabledPeriodic();
+    m_robotContainer.disabledPeriodic();
   }
 
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
