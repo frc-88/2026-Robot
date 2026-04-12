252da4177ac5dde1fa948d5314026f47951c2599
AymenAbbassi <aymenna2026@bridge-rayn.org>
2026-04-12 12:18:17 -0400

    What was that? Did you see all those weird commit messages? I don't know what that was about.
    SOrry.

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -36,8 +36,6 @@ import org.littletonrobotics.junction.AutoLogOutput;
 // circumnavigate the world
 // to keep you in sight
 
-//Subystem for clarity.
-
 public class Turret extends SubsystemBase {
   // motors & devices
   private final TalonFX m_turret = new TalonFX(Constants.TURRET_MOTOR_ID, CANBus.roboRIO());
