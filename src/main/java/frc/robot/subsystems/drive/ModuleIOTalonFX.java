7bbdeca4626465e061fbd5b110816a6d2b7b63d4
TJ Squared <navtges@gmail.com>
2026-05-01 08:35:29 -0500

    raise drive supply current limit to 65 from 55

--- src/main/java/frc/robot/subsystems/drive/ModuleIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/drive/ModuleIOTalonFX.java
@@ -110,7 +110,7 @@ public class ModuleIOTalonFX implements ModuleIO {
     driveConfig.CurrentLimits.StatorCurrentLimitEnable = true;
 
     driveConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
-    driveConfig.CurrentLimits.SupplyCurrentLimit = 55.0;
+    driveConfig.CurrentLimits.SupplyCurrentLimit = 65.0;
     driveConfig.CurrentLimits.SupplyCurrentLowerTime = 0.0;
 
     driveConfig.MotorOutput.Inverted =
