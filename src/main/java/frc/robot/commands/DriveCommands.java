48bf0005d4f01710b338fa23aec7726e9705da27
TJ Squared <navtges@gmail.com>
2026-05-30 09:09:10 -0400

    change accel limit back to 5

--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -54,8 +54,8 @@ public class DriveCommands {
   private static boolean targetSet = false;
   private static double rotationTarget;
   private static double yTarget;
-  private static SlewRateLimiter xLimiter = new SlewRateLimiter(10.0);
-  private static SlewRateLimiter yLimiter = new SlewRateLimiter(10.0);
+  private static SlewRateLimiter xLimiter = new SlewRateLimiter(5.0);
+  private static SlewRateLimiter yLimiter = new SlewRateLimiter(5.0);
   private static SlewRateLimiter rotationLimiter = new SlewRateLimiter(50.0);
 
   private DriveCommands() {}
