80ef1dc45cd98a93e583fa80cf31cdbbde893ed4
TJ2 <tj2@tj2.org>
2026-08-13 20:36:18 -0400

    Hot Tub. Maybe an order of magnitude off. Sorry.

--- src/main/java/frc/robot/subsystems/HotTub.java
+++ src/main/java/frc/robot/subsystems/HotTub.java
@@ -45,7 +45,7 @@ public class HotTub extends SubsystemBase {
       new DoublePreferenceConstant("Spinner/SpinnerSpeed", 90.0);
   private final MotionMagicPIDPreferenceConstants p_spinnerConfigConstants =
       new MotionMagicPIDPreferenceConstants(
-          "Spinner/SpinnerMotors", 0., 0., 0., 0., 0., 0., 0.011, 0., 0.);
+          "Spinner/SpinnerMotors", 0., 0., 0., 0., 0., 0., 0.11, 0., 0.);
 
   private final SysIdRoutine m_sysIdRoutine =
       new SysIdRoutine(
